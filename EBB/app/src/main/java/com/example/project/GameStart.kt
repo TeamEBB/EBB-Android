b4a88115899e05aa35ecf0b3073f48f654b296d9
lee989898 <wnsrud1352@gmail.com>
2021-09-09 17:12:05 +0900

    나무 이미지 다섯가지 추가

--- EBB/app/src/main/java/com/example/project/GameStart.kt
+++ EBB/app/src/main/java/com/example/project/GameStart.kt
@@ -7,12 +7,26 @@ import com.example.project.databinding.ActivityGameStartBinding
 
 class GameStart : AppCompatActivity() {
 
+    var score = 0
+
     lateinit var binding: ActivityGameStartBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var treeImage = when(score){
+            0 -> R.drawable.tree1
+            100 -> R.drawable.tree2
+            200 -> R.drawable.tree3
+            300 -> R.drawable.tree4
+            else -> R.drawable.tree5
+        }
+
+        binding.changeTree.setImageResource(treeImage)
+
+
 
         binding.btnLogout.setOnClickListener {
             finish()
