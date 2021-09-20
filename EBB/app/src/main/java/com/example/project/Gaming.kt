d4f5cf47916837e0d010098e84e476785bdc89bb
lee989898 <wnsrud1352@gmail.com>
2021-09-20 11:39:08 +0900

    drawable

--- EBB/app/src/main/java/com/example/project/Gaming.kt
+++ EBB/app/src/main/java/com/example/project/Gaming.kt
@@ -1,28 +1,47 @@
 package com.example.project
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
 import android.view.View
-import com.example.project.databinding.ActivityGameStartBinding
-import com.example.project.databinding.ActivityGamingBinding
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.customview.widget.ViewDragHelper
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_gaming.*
+
+
 
 class Gaming : AppCompatActivity() {
 
-    lateinit var binding: ActivityGamingBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityGamingBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_gaming)
 
+        parentCoordinatorLayout.addDraggableChild(draggableCard1)
+        parentCoordinatorLayout.addDraggableChild(draggableCard2)
+        parentCoordinatorLayout.addDraggableChild(draggableCard3)
+        parentCoordinatorLayout.addDraggableChild(draggableCard4)
 
+        parentCoordinatorLayout.setViewDragListener(object : DraggableCoordinatorLayout.ViewDragListener {
+            override fun onViewCaptured(view: View, i: Int) {
 
+                when (view.id) {
+                    R.id.draggableCard1 -> draggableCard1.isDragged = true
+                    R.id.draggableCard2 -> draggableCard2.isDragged = true
+                    R.id.draggableCard3 -> draggableCard3.isDragged = true
+                    R.id.draggableCard4 -> draggableCard4.isDragged = true
+                }
+            }
+
+            override fun onViewReleased(view: View, v: Float, v1: Float) {
+
+                when (view.id) {
+                    R.id.draggableCard1 -> draggableCard1.isDragged = false
+                    R.id.draggableCard2 -> draggableCard2.isDragged = false
+                    R.id.draggableCard3 -> draggableCard3.isDragged = false
+                    R.id.draggableCard4 -> draggableCard4.isDragged = false
+                }
+            }
+        })
     }
-
-
-
 }
